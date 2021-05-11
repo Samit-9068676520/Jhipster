371f1c5aa8010fda671482b552d6d42ff31a66be
Samit-123 <samitverma279@gmail.com>
2021-05-11 16:59:11 +0530

    Initial version of firstprojectwithjhipster generated by JHipster-7.0.1

---
+++ src/main/java/com/microservice/demo/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.microservice.demo.web.rest;
---
+++ src/main/java/com/microservice/demo/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.microservice.demo.web.rest.vm;
